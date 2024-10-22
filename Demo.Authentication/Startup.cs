2984a47c27dd5f4fd7a09d96f46bfb033a15dc29
1 <1>
2024-10-22 13:37:58 +0300

    Update

--- Demo.Authentication/Startup.cs
+++ Demo.Authentication/Startup.cs
@@ -1,18 +1,9 @@
-using System.Text;
-using Demo.Authentication.Authentication;
 using Demo.Authentication.Authorization;
 using Demo.Authentication.Data;
 using Demo.Authentication.Middleware;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Demo.Authentication
@@ -30,8 +21,6 @@ namespace Demo.Authentication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/dist"; });
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
@@ -95,10 +84,6 @@ namespace Demo.Authentication
             app.UseCors("Cors");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-            }
 
             app.UseRouting();
 
@@ -114,22 +99,6 @@ namespace Demo.Authentication
                         name: "default",
                         pattern: "{controller}/{action=Index}/{id?}");
                 });
-
-            /*
-            app.UseSpa(
-                spa =>
-                {
-                    // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                    // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                    spa.Options.SourcePath = "ClientApp";
-
-                    if (env.IsDevelopment())
-                    {
-                        spa.UseAngularCliServer(npmScript: "start");
-                    }
-                });
-                */
         }
     }
 }
